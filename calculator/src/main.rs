247ec39af17d466937d85d7bbed782df23df44e6
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2023-02-18 22:07:56 -0300

    [IMPROVEMENT] - rusty - master

--- calculator/src/main.rs
+++ calculator/src/main.rs
@@ -2,22 +2,15 @@ use std::env::{args, Args};
 
 fn main() {
     let  mut args:Args= args();
-    println!("Total of args received: {:#?}", args);
-
-    println!("Args before nth: {:?}",args);
 
     let first_arg:String = args.nth(1).unwrap();
-    println!("Args after nth(1): {:?}",args);
 
     let operator:char = args.nth(0).unwrap().chars().next().unwrap();
     let second_arg:String = args.nth(0).unwrap();
-    println!("{:?} {:?} {:?}",first_arg, operator, second_arg);
 
     
     let first_number:f32 = first_arg.parse().unwrap();
     let second_number= second_arg.parse::<f32>().unwrap();
-
-    println!("{:?} {:?} {:?}",first_number, operator, second_number);
 
     let result = operate(operator, first_number, second_number);
 
