4b6a4c8e36c65211d9370e08a948c3d5e02de344
Gabriel Cabral <gabriel.alonso.dev@icloud.com>
2023-02-21 13:26:03 -0300

    [FIX] - rusty - wheater-api

--- openSSL/target/debug/build/quote-cb5ed9c287cfdf99/build_script_build-cb5ed9c287cfdf99.d
+++
@@ -1,5 +0,0 @@
-/Users/gabrielalonsocabral/studyspace/rusty/openSSL/target/debug/build/quote-cb5ed9c287cfdf99/build_script_build-cb5ed9c287cfdf99: /Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs
-
-/Users/gabrielalonsocabral/studyspace/rusty/openSSL/target/debug/build/quote-cb5ed9c287cfdf99/build_script_build-cb5ed9c287cfdf99.d: /Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs
-
-/Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/quote-1.0.23/build.rs:
--- openSSL/target/debug/deps/cfg_if-92378a527f85b23d.d
+++
@@ -1,5 +0,0 @@
-/Users/gabrielalonsocabral/studyspace/rusty/openSSL/target/debug/deps/cfg_if-92378a527f85b23d.rmeta: /Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/gabrielalonsocabral/studyspace/rusty/openSSL/target/debug/deps/cfg_if-92378a527f85b23d.d: /Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs
-
-/Users/gabrielalonsocabral/.cargo/registry/src/github.com-1ecc6299db9ec823/cfg-if-1.0.0/src/lib.rs:
